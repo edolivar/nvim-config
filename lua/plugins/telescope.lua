a612c909e63b2094fb8755a3a66cafaf81b6ffbc
Ghost Typer <edwinolivares123@gmail.com>
2025-06-19 09:59:46 -0700

    filter out certain .folders

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules/",
+          "%.git/",
+          "dist/",
+          "build/",
+        },
+      },
+    })
+  end,
+}
